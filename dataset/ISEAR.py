f1175f21efaef144885bb7937145a332d0c81132
yash upadhyay <yashup1997@gmail.com>
2020-04-23 18:57:46 +0530

    splitting X and y

--- dataset/ISEAR.py
+++ dataset/ISEAR.py
@@ -1,11 +1,22 @@
+import warnings 
+warnings.filterwarnings('ignore')
 import pandas as pd 
 import numpy as np
 import regex as re
+from keras.preprocessing.text import Tokenizer 
+from keras.preprocessing.sequence import pad_sequences
+import json
+import keras.utils as util
+
 
 class Isear():
     
     def load(self,path):
-        data = pd.read_csv(path,header = None)
-        print(data[2][2])
+        data = pd.read_csv(path,header = 0)
+        y_target=data["emotion"]
+        x_input=data["text"]
+
+    def basic_preprocess(self,text):
+        
 
 

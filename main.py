921740394d35620e667038022ec887415534bc9a
yash upadhyay <yashup1997@gmail.com>
2020-04-28 18:12:48 +0530

    loading data from isear and jigsaw

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+import warnings 
+warnings.filterwarnings('ignore')
 from dataset.ISEAR import Isear
+from dataset.jigsaw import Jigsaw
 
-dat = Isear()
-dat.load('data/isear/ISEAR.csv')
+data1 = Isear()
+
+train_x,train_y,eval_x  , eval_y, test_x , test_y = data1.load()
+print('shape of isear')
+print(train_x.shape)
+print(train_y.shape)
+print(eval_x.shape)
+print(eval_y.shape)
+print(test_x.shape)
+print(test_y.shape)
+
+data2 = Jigsaw()
+
+train_x,train_y,eval_x  , eval_y, test_x , test_y = data2.load()
+print('shape of toxic comment data')
+print(train_x.shape)
+print(train_y.shape)
+print(eval_x.shape)
+print(eval_y.shape)
+print(test_x.shape)
+print(test_y.shape)
+

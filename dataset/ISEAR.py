0cb7217978aa6c00a40c415cb408d1ad490ca9e3
yash upadhyay <yashup1997@gmail.com>
2020-04-28 18:03:01 +0530

    train test split for ISEAR dataset

--- dataset/ISEAR.py
+++ dataset/ISEAR.py
@@ -3,20 +3,47 @@ warnings.filterwarnings('ignore')
 import pandas as pd 
 import numpy as np
 import regex as re
-from keras.preprocessing.text import Tokenizer 
-from keras.preprocessing.sequence import pad_sequences
-import json
-import keras.utils as util
-
+from sklearn import preprocessing
+from keras.utils import to_categorical
 
 class Isear():
     
-    def load(self,path):
-        data = pd.read_csv(path,header = 0)
-        y_target=data["emotion"]
-        x_input=data["text"]
-
-    def basic_preprocess(self,text):
+    def load(self):
+        train = pd.read_csv('data/isear/ISEAR.csv')
+        embed_size=0
+        y = train['emotion'].values
+        list_sentences_train = train["text"]
+        max_features = 20000
+        tokenizer = Tokenizer(num_words=max_features)
+        tokenizer.fit_on_texts(list(list_sentences_train))
+        list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
+        maxlen = 200
+        X_t = pad_sequences(list_tokenized_train, maxlen=maxlen)
+        le = preprocessing.LabelEncoder()
+        le.fit(y)
+        y = le.transform(y)
         
+        y = to_categorical(y)
+        train_x , x , train_y , y = train_test_split(X_t , y , 
+                                            test_size = 0.3 ,
+                                            random_state = 324)
+
+        eval_x , test_x , eval_y , test_y = train_test_split(x , y , 
+                                                            test_size = 0.5 , 
+                                                            random_state = 324)
+
+        return train_x,train_y,eval_x  , eval_y, test_x , test_y
+
+
+
+    
+
+
+
+
+        
+
+
+
 
 

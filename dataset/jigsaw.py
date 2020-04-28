edcd0837cc30e0e6d701391825615465a32a74a0
yash upadhyay <yashup1997@gmail.com>
2020-04-28 17:34:35 +0530

    tokenizer

--- dataset/jigsaw.py
+++ dataset/jigsaw.py
@@ -1,10 +1,16 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
+ 
 
 class Jigsaw:
     def load(self,path_2_data):
         train = pd.read_csv('data/jigsaw/train/train.csv')
-        x = train["comment_text"].fillna("CVxTz").value
-        y = train[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].values
+        list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+        y = train[list_classes].values
+        list_sentences_train = train["comment_text"]
+        max_features = 20000
+        tokenizer = Tokenizer(num_words=max_features)
+        tokenizer.fit_on_texts(list(list_sentences_train))
+        list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
 
         return x,y
